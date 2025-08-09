13b39bec26ee31169ddd131d5af309e7c10aa2c7
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2025-08-09 09:59:21 -0400

    feat: remove upper limits for tf providers

--- version.tf
+++ version.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.9.0, < 6.0.0"
+      version = ">= 4.9.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = ">= 3.0.0, < 4.0.0"
+      version = ">= 3.0.0"
     }
   }
 }
